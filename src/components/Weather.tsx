3ad8d7f7fad7551e2ea50496e9f3de78ba370e2b
Mbonu Basil <mbonubasil@gmail.com>
2024-07-14 23:35:41 -0400

    Add weather elements

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,4 +1,4 @@
-import { Search } from "lucide-react";
+import { Search, Sun, Waves, Wind } from "lucide-react";
 
 export default function Weather() {
     return (
@@ -7,7 +7,25 @@ export default function Weather() {
                 <input type="text" placeholder="Search" />
                 <Search size={50} color="blue" className="search-icon" />
             </div>
-            <h1>Weather</h1>
+            <Sun size={100} color="orange" className="weather-icon" />
+            <p className="temperature">23°C</p>
+            <p className="location">Toronto</p>
+            <div className="weather-details">
+                <div className="col">
+                    <Waves color="white" className="icon" />
+                    <div>
+                        <p>70</p>
+                        <span>Humidity</span>
+                    </div>
+                </div>
+                <div className="col">
+                    <Wind color="white" className="icon" />
+                    <div>
+                        <p>90 km/h</p>
+                        <span>Wind Speed</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
