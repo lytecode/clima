e80a7547a022a84353d58e1080a68c3277dd853c
Mbonu Basil <mbonubasil@gmail.com>
2024-07-15 00:54:29 -0400

    Fetch data from openWeather api

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,6 +1,37 @@
 import { Search, Sun, Waves, Wind } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function Weather() {
+    const [weatherData, setWeatherData] = useState({
+        location: '',
+        windSpeed: '',
+        temperature: 0,
+        humidity: '',
+        icon: '',
+    });
+
+    const getData = async (city: string) => {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${import.meta.env.VITE_OPEN_WEATHER_API_KEY}`
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+            setWeatherData({
+                location: data.name,
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp),
+                humidity: data.main.humidity,
+                icon: data.weather[0].icon,
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getData('Toronto')
+    }, [])
+
     return (
         <div className="weather">
             <div className="search-bar">
@@ -8,20 +39,20 @@ export default function Weather() {
                 <Search size={50} color="blue" className="search-icon" />
             </div>
             <Sun size={100} color="orange" className="weather-icon" />
-            <p className="temperature">23°C</p>
-            <p className="location">Toronto</p>
+            <p className="temperature">{weatherData.temperature}°C</p>
+            <p className="location">{weatherData.location}</p>
             <div className="weather-details">
                 <div className="col">
                     <Waves color="white" className="icon" />
                     <div>
-                        <p>70</p>
+                        <p>{weatherData.humidity}</p>
                         <span>Humidity</span>
                     </div>
                 </div>
                 <div className="col">
                     <Wind color="white" className="icon" />
                     <div>
-                        <p>90 km/h</p>
+                        <p>{weatherData.windSpeed} km/h</p>
                         <span>Wind Speed</span>
                     </div>
                 </div>
